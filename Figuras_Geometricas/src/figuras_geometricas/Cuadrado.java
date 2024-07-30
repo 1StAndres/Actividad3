a679ac81667b53fd050fd8faf04b8edeb6de22e5
1StAndres <160525045+1StAndres@users.noreply.github.com>
2024-07-30 14:58:30 -0500

    Add files via upload

---
+++ Figuras_Geometricas/src/figuras_geometricas/Cuadrado.java
@@ -0,0 +1,13 @@
+package figuras_geometricas;
+public class Cuadrado {
+    int lado;
+    public Cuadrado(int lado) {
+        this.lado = lado;
+    }
+    double calcularArea() {
+        return lado*lado;
+    }
+    double calcularPerímetro() {
+        return (4*lado);
+    }
+}
---
+++ Figuras_Geometricas/src/figuras_geometricas/PruebaFiguras.java
@@ -0,0 +1,9 @@
+package figuras_geometricas;
+public class PruebaFiguras {
+    public static void main(String[] args) {
+        Pantalla pant =new Pantalla();
+        pant.setVisible(true);
+        pant.setLocationRelativeTo(null);
+    }
+    
+}
